3d6eb702779c484d5d3768d06d3df9df650ab737
Maribel Obando <maribel-obando.j@utp.edu.co>
2024-10-09 00:10:30 -0500

    doing improvements

---
+++ gestorLaboral/gestorLaboral/Services/IEmployees.cs
@@ -0,0 +1,13 @@
+﻿using UtilitiesRestaurante;
+
+namespace gestorLaboral.Services
+{
+	public interface IEmployees
+	{
+		Task<List<EmployeeRequest>> Lista();
+		Task<EmployeeRequest> Buscar(int Cc);
+		Task<int> Guardar(EmployeeRequest empleado);
+		Task<int> Editar(EmployeeRequest empleado);
+		Task<bool> Eliminar(int Cc);
+	}
+}
